e581b9ddcc45069acba67ee418b0c1dd44785b8c
Aysenur Mayuk <amayuk@k1m32s02.42istanbul.com.tr>
2024-10-17 20:09:36 +0300

    tilde

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: amayuk <amayuk@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 18:42:48 by amayuk            #+#    #+#             */
-/*   Updated: 2024/10/11 18:44:25 by amayuk           ###   ########.fr       */
+/*   Updated: 2024/10/17 20:08:30 by amayuk           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,40 +30,40 @@ static void	update_env_var(t_env **env_list, char *key, char *value)
 	add_env_node(env_list, key, value);
 }
 
-void	ft_cd(t_cmd *cmd)
+static void	change_to_home(t_cmd *cmd)
 {
-	char	*oldpwd;
-	char	cwd[1024];
 	char	*home;
 
-	oldpwd = NULL;
-	oldpwd = get_env(cmd, "PWD", NULL);
-	if (!cmd->command[0][1])
+	home = get_env(cmd, "HOME", NULL);
+	if (home)
 	{
-		home = get_env(cmd, "HOME", NULL);
-		if (home)
-		{
-			if (chdir(home) != 0)
-			{
-				printf("cd: %s: %s\n", home, strerror(errno));
-			}
-		}
+		if (chdir(home) != 0)
+			printf("cd: %s: %s\n", home, strerror(errno));
 	}
-	else
-	{
-		if (chdir(cmd->command[0][1]) != 0)
-		{
-			printf("cd: %s: %s\n", cmd->command[0][1], strerror(errno));
-		}
-	}
+}
+
+static void	update_pwd(t_cmd *cmd, char *oldpwd)
+{
+	char	cwd[1024];
+
 	if (getcwd(cwd, sizeof(cwd)) != NULL)
 	{
 		update_env_var(&(cmd->env), "OLDPWD", oldpwd);
 		update_env_var(&(cmd->env), "PWD", cwd);
 	}
 	else
-	{
 		printf("cd: error retrieving current directory: %s\n", strerror(errno));
-	}
+}
+
+void	ft_cd(t_cmd *cmd)
+{
+	char	*oldpwd;
+
+	oldpwd = get_env(cmd, "PWD", NULL);
+	if (!cmd->command[0][1] || ft_strcmp(cmd->command[0][1], "~") == 0)
+		change_to_home(cmd);
+	else if (chdir(cmd->command[0][1]) != 0)
+		printf("cd: %s: %s\n", cmd->command[0][1], strerror(errno));
+	update_pwd(cmd, oldpwd);
 	free(oldpwd);
 }
